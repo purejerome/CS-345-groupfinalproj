75e42478210502e412f9b34ec34e78e4713ed09b
Jerome <donfacjc@dukes.jmu.edu>
2024-03-23 12:56:09 -0400

    scroll bar and nav bar

---
+++ scripts/pagescrolling.js
@@ -0,0 +1,8 @@
+// const progress = document.querySelector('.progress-bar');
+// const totalHeight = document.body.scrollHeight - window.innerHeight;
+
+// function scrolling() {
+//     let progressHeight = (window.scrollY / totalHeight) * 100;
+//     progress.style.height = progressHeight + "%";
+// }
+// window.addEventListener('scroll', scrolling);
