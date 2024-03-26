7d7434c25865c7436542afc23ef17cac04affefa
ShadowKnight <hamiltnc@dukes.jmu.edu>
2024-03-26 09:43:53 -0400

    Basic Login + Registration
    Added a basic Login and Registration page that when scripted should perform the tasks they are meant for. TODO: Align Text Boxes and Input Fields to look more professional.
    TODO: Change Text Boxes and buttons to look better than basic HTML elements.
    Registration Page Look is subject to change.

---
+++ scripts/loginScript.js
@@ -0,0 +1,1 @@
+console.log("Good Evening People of the Great Country of the United States of America I hope you are all ready for a glorious day on this fine Earth that we find ourselves Residing In!")
