a771c7516286953c68baf34a1ec5c92cc5d42b7c
ShadowKnight <hamiltnc@dukes.jmu.edu>
2024-04-25 13:10:09 -0400

    Radio buttons are now exclusive
    The radio buttons on register page will now hide the alternate method when selected. So clicking phone will hide email related fields and vice versa.

--- scripts/loginScript.js
+++ scripts/loginScript.js
@@ -1,1 +1,21 @@
 console.log("Good Evening People of the Great Country of the United States of America I hope you are all ready for a glorious day on this fine Earth that we find ourselves Residing In!")
+
+
+
+function registerButton() {
+    phoneButton = document.getElementById("ContactPhone");
+    emailButton = document.getElementById("ContactEmail");
+    if (phoneButton.checked) {
+        document.getElementById("phone").style.display = "block";
+        document.getElementById("phoneText").style.display = "block";
+        document.getElementById("email").style.display = "none";
+        document.getElementById("emailText").style.display = "none";
+    }
+
+    if (emailButton.checked) {
+        document.getElementById("phone").style.display = "none";
+        document.getElementById("phoneText").style.display = "none";
+        document.getElementById("email").style.display = "block";
+        document.getElementById("emailText").style.display = "block";
+    }
+}
