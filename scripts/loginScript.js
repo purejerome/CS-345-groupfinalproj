93c42fed07cfbe61176d036e70caa2acfa070b64
ShadowKnight <hamiltnc@dukes.jmu.edu>
2024-04-27 11:47:35 -0400

    RegisterForm 90% Complete
    Really all that is next for Register is to move the console logged user's information to an array that gets placed into Local Storage.

--- scripts/loginScript.js
+++ scripts/loginScript.js
@@ -2,11 +2,12 @@ console.log("Good Evening People of the Great Country of the United States of Am
 
 
 
-function registerButton() {
-    phoneButton = document.getElementById("ContactPhone");
-    emailButton = document.getElementById("ContactEmail");
+function registerRadio() {
+    let phoneButton = document.getElementById("ContactPhone");
+    let emailButton = document.getElementById("ContactEmail");
     if (phoneButton.checked) {
         document.getElementById("phone").style.display = "block";
+        document.getElementById("phone").value = "";
         document.getElementById("phoneText").style.display = "block";
         document.getElementById("email").style.display = "none";
         document.getElementById("emailText").style.display = "none";
@@ -16,6 +17,38 @@ function registerButton() {
         document.getElementById("phone").style.display = "none";
         document.getElementById("phoneText").style.display = "none";
         document.getElementById("email").style.display = "block";
+        document.getElementById("email").value = "";
         document.getElementById("emailText").style.display = "block";
+
     }
+}
+
+function register(event) {
+    event.preventDefault();
+    let username = document.getElementById("user").value;
+    let password = document.getElementById("pass").value;
+    let confpass = document.getElementById("confirmpass").value;
+    let emailButton = document.getElementById("ContactEmail");
+    let phoneButton = document.getElementById("ContactPhone");
+    let emailText = document.getElementById("email").value;
+    let phoneText = document.getElementById("phone").value;
+    if (password == confpass && password.length > 0) {
+        console.log("passwords are matching");
+        if (username.length > 0) {
+            if (emailButton.checked && emailText.length > 0) {
+                let userInfo = [username, password, emailText];
+                console.log(userInfo);
+            } else if (phoneButton.checked && phoneText.length > 0) {
+                let userInfo = [username, password, phoneText];
+                console.log(userInfo);
+            } else {
+                alert("Either email or phone must content in them");
+            }
+        }
+    } else if (password.length == 0 || confpass.length == 0) {
+        alert("Both passwords must have content in them to proceed");
+    } else {
+        alert("Passwords do not match, please reenter them");l
+    }
+
 }
