955634e05cf7a14708d49013cde06a5bbeb77ac2
Jerome <donfacjc@dukes.jmu.edu>
2024-04-28 15:20:14 -0400

    mobile click changes?

--- scripts/cscrolling.js
+++ scripts/cscrolling.js
@@ -2,13 +2,11 @@ const lefts = document.querySelectorAll('.prev-button');
 const rights = document.querySelectorAll('.next-button');
 const carousels = document.querySelectorAll('.cSection');
 
-function toggleVis(event, left, right) {
+function toggleVis(event) {
     const cSection = event.target;
     const carousel = cSection.parentNode;
-    console.log(cSection);
-    console.log(cSection.parentNode);
     const lefty = carousel.querySelector('.prev-button');
-    console.log(lefty);
+
     const righty = carousel.querySelector('.next-button');
     if (cSection.scrollLeft == 0) {
         lefty.classList.add('gone');
@@ -16,11 +14,9 @@ function toggleVis(event, left, right) {
     else {
         lefty.classList.remove('gone');
     }
-    console.log("Left:" + cSection.scrollLeft);
-    console.log("Width:" + cSection.clientWidth);
+
     const realMove = cSection.scrollLeft + cSection.clientWidth;
-    console.log("Moved:" + realMove);
-    console.log("Total: " + cSection.scrollWidth);
+
     if (realMove >= cSection.scrollWidth - 1) {
         righty.classList.add('gone');
     }
@@ -29,6 +25,7 @@ function toggleVis(event, left, right) {
     }
 }
 function leftClick(event) {
+    event.preventDefault();
     const cSection = event.target.closest('.carousel').querySelector('.cSection');
     cSection.scrollBy({
         left: -cSection.clientWidth,
@@ -36,6 +33,7 @@ function leftClick(event) {
     });
 }
 function rightClick(event) {
+    event.preventDefault();
     const cSection = event.target.closest('.carousel').querySelector('.cSection');
     cSection.scrollBy({
         left: cSection.clientWidth,
@@ -44,14 +42,10 @@ function rightClick(event) {
 }
 for (let l of lefts) {
     l.addEventListener('click', leftClick);
-    l.addEventListener('touchend', leftClick);
 }
 for (let r of rights) {
     r.addEventListener('click', rightClick);
-    r.addEventListener('touchend', rightClick);
 }
 for (let c of carousels) {
-    const left = c.closest('.prev-button');
-    const right = c.closest('next-button');
     c.addEventListener('scroll', toggleVis);
 }
